f0e3df1cab2b9d97121b304eda009917db67b81b
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-13 22:08:21 +0530

    [Prashant]Add: Ability to Check Number is even or odd

--- ProgrammingConstructs/ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/ProgrammingConstructs/Program.cs
@@ -7,13 +7,17 @@ namespace ProgrammingConstructs
         {
             Console.WriteLine("Welcome to C# Programming");
             Console.WriteLine("Choose any options to perform");
-            Console.WriteLine("1:EqualOrNot");
+            Console.WriteLine("1:EqualOrNot\n2: EvenOrOdd");
             int options = Convert.ToInt32(Console.ReadLine());
             switch(options)
             {
                 case 1:
                     EqualOrNot equalOrNot = new EqualOrNot();
                     equalOrNot.EqualOrNotPrint();
+                    break;
+                case 2:
+                    EvenOrOdd evenOrOdd = new EvenOrOdd();
+                    evenOrOdd.EvenOddPrint();
                     break;
             }
            
