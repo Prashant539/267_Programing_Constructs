7134ae5a76524f78bfcc382072b38e070056ecff
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-13 20:18:21 +0530

    [Prashant]Add: Check Two numbers are equal or not

---
+++ ProgrammingConstructs/ProgrammingConstructs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ProgrammingConstructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Programming");
+            Console.WriteLine("Choose any options to perform");
+            Console.WriteLine("1:EqualOrNot");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch(options)
+            {
+                case 1:
+                    EqualOrNot equalOrNot = new EqualOrNot();
+                    equalOrNot.EqualOrNotPrint();
+                    break;
+            }
+           
+        }
+    }
+}
